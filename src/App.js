0aa2ffccdd882683e458fcd34bc40c4ab882f7b1
ASC Studio <asc@adamsmith.ie>
2021-01-14 19:03:59 +0000

    style: Addition of react-spring annimations

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
+import Helmet from "react-helmet";
 import logo from './logo.svg';
 import './App.css';
+import {useSpring, animated} from 'react-spring';
+import backgroundVideo from './plants.mp4';
+import backgroundVideoThumb from './plants.png';
 
 function App() {
+  const props = useSpring({
+      opacity: 1, 
+      from: {opacity: 0}, 
+      config: {delay: 10, duration: 2000},
+      });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Helmet>
+         {/* <!-- HTML Meta Tags --> */}
+        <title>Bunús Coffee, Dublin</title>
+        <meta name="description" content="Proper Coffee, Treats & more coming to Dublin's Northside in early 2021." />
+
+        {/* <!-- Google / Search Engine Tags --> */}
+        <meta itemprop="name" content="Bunús Coffee, Dublin" />
+        <meta itemprop="description" content="Proper Coffee, Treats & more coming to Dublin's Northside in early 2021." />
+        <meta itemprop="image" content="https://bunus.netlify.app/static/media/logo.fcd4cf9c.svg" />
+
+        {/* <!-- Facebook Meta Tags --> */}
+        <meta property="og:url" content="http://bunus.netlify.app" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Bunús Coffee, Dublin" />
+        <meta property="og:description" content="Proper Coffee, Treats & more coming to Dublin's Northside in early 2021." />
+        <meta property="og:image" content="https://bunus.netlify.app/static/media/logo.fcd4cf9c.svg" />
+
+        {/* <!-- Twitter Meta Tags --> */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Bunús Coffee, Dublin" />
+        <meta name="twitter:description" content="Proper Coffee, Treats & more coming to Dublin's Northside in early 2021. " />
+        <meta name="twitter:image" content="https://bunus.netlify.app/static/media/logo.fcd4cf9c.svg" />
+      </Helmet>
+      <main className="App-header">
+      <video playsinline autoPlay muted loop poster={backgroundVideoThumb} id="bgvid">
+        <source src={backgroundVideo} type="video/mp4" />
+      </video>
+        <animated.div style={props}><img src={logo} className="App-logo" alt="logo" /></animated.div>
         <p>
           Proper Coffee, delicious treats and more.. 
         </p>
         <span className="Coming"> 
           Coming to <br /> Dublin's Northside early 2021.
         </span>
-      </header>
+      </main>
     </div>
   );
 }
--- src/Follow.js
+++ src/Follow.js
@@ -1,26 +1,36 @@
 import './Follow.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import {useSpring, animated} from 'react-spring';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 function Follow() {
+    const props = useSpring({
+      opacity: 1, 
+      from: {opacity: 0}, 
+      config: {delay: 0, duration: 1000},
+      });
   return (
+    <animated.div style={props}>
     <div className="Follow">
-        <p className="Follow-content"> 
-          Follow us for more information
-        </p>
-        <a
-          className="Bunus-instagram"
-          href="https://www.instagram.com/bunuscoffee/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         <FontAwesomeIcon icon={faInstagram} />
-         <span>@bunuscoffee</span>
-        </a>
+        <div className="Follow-inner">
+            <p className="Follow-content"> 
+            Follow us for more information
+            </p>
+            <a
+            className="Bunus-instagram"
+            href="https://www.instagram.com/bunuscoffee/"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            <FontAwesomeIcon icon={faInstagram} />
+            <span>@bunuscoffee</span>
+            </a>
+        </div>
         <p className="Copyright">
             ©2020 Bunús Coffee
         </p>
     </div>
+    </animated.div>
   );
 }
 
